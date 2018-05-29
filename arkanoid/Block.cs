61021d193e98475fd8d046eeae2764bb5a92b867
JiggleWigglesnicker <games.j@live.nl>
2018-05-29 13:19:10 +0200

    block class created

---
+++ arkanoid/Block.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+public enum Color { Black = 0, Yellow, Blue, Green, Purple };
+public class Block
+{
+    private int hitCount;
+    private int maxHits;
+    Color _color;
+	public Block(int maxHits, Color _color)
+	{
+        this.maxHits = maxHits;
+        this._color = _color;
+	}
+
+    public void setMaxHits(int maxHits)
+    {
+       
+    }
+
+    public int getMaxHits()
+    {
+        return maxHits;
+    }
+
+    public void setColor(Color blockColor)
+    {
+        _color = blockColor;
+    }
+
+    public Color getColor()
+    {
+        return _color;
+    }
+
+    public void addHit()
+    {
+        hitCount += 1;
+    }
+
+    public string getUpgrade()
+    {
+        return null;
+    }
+}
